b251fe86c907e73b447dd48d21bc2eaa45b40ff2
unknown <somono65@gmail.com>
2023-09-15 14:20:25 +0600

    change over

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,12 +9,19 @@ const Cart = (props) => {
         total = total + product.price;
     }
 
+    const shipping = 15;
+    const tax = (total + shipping) * 0.10;
+    const grandTotal = total+ shipping+ tax;
+
     return (
         <div>
              <h3>Order Summary</h3>
              <h5>Item Ordered: {props.cart.length}</h5>
              <br/>
-             <p>Total: {total}</p>
+             <p>Total: {total.toFixed(2)}</p>
+             <p>Shipping: {shipping}</p>
+             <p>Tax: {tax.toFixed(2)}</p>
+             <p>Grand Total: {grandTotal.toFixed(2)}</p>
         </div>
     );
 };
