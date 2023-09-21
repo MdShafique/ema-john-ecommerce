e979af170d2f22731de46c8683b1a82ec075c8cc
unknown <somono65@gmail.com>
2023-09-21 14:47:21 +0600

    okay kaj kora hoiche

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,24 +8,30 @@ const Shop = () => {
 
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
+    const [displayProducts, setDislpayProducts] = useState([]);
 
     useEffect(()=>{
-        console.log('Product api called');
         fetch('./products.JSON')
         .then(res =>res.json())
-        .then(data=> {setProducts(data);
-            console.log('product recieved');
+        .then(data=> {
+            setProducts(data);
+            setDislpayProducts(data);
+
         });
     }, []);
 
     useEffect(()=>{
-        console.log('LocalStorage cart called')
         if(products.length){
             const saveCart = getStoredCart();
             const storeCart =[];
             for(const key in saveCart){
                 const addedProduct = products.find(product=>product.key===key);
-                storeCart.push(addedProduct);
+                if(addedProduct){
+                    const quantity = saveCart[key];
+                    addedProduct.quantity = quantity;
+                    storeCart.push(addedProduct);
+                }
+                
             }
             setCart(storeCart);
         }
@@ -37,12 +43,24 @@ const Shop = () => {
         addToDb(product.key);
     }
 
+    const handlesearch = event =>{
+        const searchText = event.target.value;
+        const matchProducts = products.filter(product=>product.name.toLowerCase().includes(searchText.toLowerCase()));
+        setDislpayProducts(matchProducts);
+    }
+
 
     return (
-        <div className='shop-container'>
+        <>
+            <div className='search-container'>
+                <input type='text'
+                onChange={handlesearch}
+                placeholder='Search Product'/>
+            </div>
+            <div className='shop-container'>
             <div className='product-container'>
                 {
-                    products.map(product=> <Product 
+                    displayProducts.map(product=> <Product 
                         key={product.key}
                         product={product}
                         handleAddToCart = {handleAddToCart}
@@ -51,10 +69,11 @@ const Shop = () => {
                         </Product>)
                 }
             </div>
-            <div className='cart-container'>
-               <Cart cart={cart}></Cart>
-            </div>
+                <div className='cart-container'>
+                <Cart cart={cart}></Cart>
+            </div> 
         </div>
+        </>
     );
 };
 
